aa91d459c3184ccaa2b8d1dd73313048915e089f
shashikala133 <sshashikala1330@gmail.com>
2025-07-15 18:30:51 +0530

    initial commit

---
+++ consumer/consumer/src/main/java/com/example/consumer/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.example.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+    public void listen1(String message){
+        System.out.println("message recieved 1: "+message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+    public void listen2(String message){
+        System.out.println("message recieved 2: "+message);
+    }
+}
